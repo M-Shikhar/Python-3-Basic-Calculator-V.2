727802c36783511f83d977ce6095b25a0280fb43
M-Shikhar <77896163+M-Shikhar@users.noreply.github.com>
2021-01-24 15:11:55 +0530

    Update main.py
    Added check for valid float input.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 def basic_calculator():
     
     # Run infinitely unless opton is 6.End
+    print("------------------------------------------------")
+    print("Hello Friend, I will calculate anything you want")
+    print("------------------------------------------------")
     while 1:
-        print("------------------------------------------------")
-        print("Hello Friend, I will calculate anything you want")
-        print("------------------------------------------------")
         print("What do you want to do?")
         print("1.Addition")
         print("2.Subtraction")
@@ -20,14 +20,27 @@ def basic_calculator():
             print("End. Thankyou for using this calculator")
             return()
             
-        # Enter 2 numbers
-        b = int(input("Enter the First Number\n"))
-        c = int(input("Enter the Second Number\n"))
-        
+        # Checking if inputs are valid float numbers or not
+        flag = 0
+        try:
+            b = float(input("Enter the First Number\n"))
+            c = float(input("Enter the Second Number\n"))
+        except:
+            print("That's not a number")
+            flag = 1
+        while flag:
+            try:
+                b = float(input("Enter the First Number\n"))
+                c = float(input("Enter the Second Number\n"))
+                flag = 0
+            except:
+                print("Those are not numbers")
+                flag = 1
+            
         # Asking user to enter valid 2nd number for division, i.e., other than 0
         while a == 4 and c == 0:
             print("You cannot divide a number by 0")
-            c = int(input("Enter the Second Number other than 0\n"))
+            c = float(input("Enter the Second Number other than 0\n"))
             
         # Addition
         if a == 1:
